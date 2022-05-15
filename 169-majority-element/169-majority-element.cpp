3e5ef0c88f006e4a36ac5256e1d46552e1a20f09
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-05-15 17:09:19 +0530

    Time: 20 ms (79.04%), Space: 19.6 MB (14.52%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+    // brute: for each elt, iterate in whole array to count number of times it occurs, if ct>n/2 return that elt
+    // TC: O(N^2) and SC: O(1)
+
+    
+    
+//     // intermidiate: TC: O(N) and SC: O(N)
 //     int majorityElement(vector<int>& a) {
 //         int n = a.size();
         
-//         int m;
-//         int ct = 0;
+//         map<int,int> mp;
 //         for(int i=0;i<n;++i){
-//             if(ct==0){
-//                 m = a[i];
-//                 ct++;
-//             }
-//             else{
-//                 if(m==a[i])ct++;
-//                 else ct--;
-//             }
+//             mp[a[i]]++;
+//             if(mp[a[i]]>n/2)return a[i];
 //         }
-        
-//         return m;
+//         return -1;  // if no majority elt 
 //     }
     
     
-//     // optimal: TC: O(N) and SC: O(1)
-//     int majorityElement(vector<int>& a) {
+    
+    
+    
+    // optimal: TC: O(N) and SC: O(1)
+    int majorityElement(vector<int>& a) {
 //         int n = a.size();
         
 //         int m = a[0];
@@ -37,18 +38,18 @@ public:
 //         }
         
 //         return m;
-//     }
-    
-    // TC: O(N) and SC: O(N)
-    int majorityElement(vector<int>& a) {
+        
         int n = a.size();
-        
-        map<int,int> mp;
+        int votes = 0;
+        int candidate = -1;
         for(int i=0;i<n;++i){
-            mp[a[i]]++;
-            if(mp[a[i]]>n/2)return a[i];
+            if(votes==0){
+                candidate = a[i];
+            }
+            
+            if(a[i]==candidate)votes++;
+            else votes--;
         }
-        return -1;  // if no majority elt
-        
+        return candidate;
     }
 };
