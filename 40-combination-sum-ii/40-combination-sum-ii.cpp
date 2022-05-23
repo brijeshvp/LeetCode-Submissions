566e1a24af466e743eb07f2fc3898eef99b0da14
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-05-23 23:06:06 +0530

    Time: 10 ms (48.32%), Space: 10.6 MB (87.68%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+//     // bruteforce -> TLE -> 172/175 test case passed
+//     // TC: O(2^n * k * log(set size));  k = average length of combinations
+//     void solve(int i,vector<int> &c,int t,set<vector<int>> &st,vector<int> &combi){
+//         if(t<0)return;
+//         if(t==0){
+//             st.insert(combi);
+//             // elts are +ve -> so going ahead will increase the sum -> thus return
+//             return;
+//         }
+//         // if t!=0 but i == n
+//         if(i==c.size())return;
+        
+//         combi.push_back(c[i]);
+//         solve(i+1,c,t-c[i],st,combi);
+//         combi.pop_back();   // backtrack
+//         solve(i+1,c,t,st,combi);
+//     }
+    
+//     vector<vector<int>> combinationSum2(vector<int>& c, int t) {
+//         set<vector<int>> st;
+//         vector<int> combi;
+//         sort(c.begin(),c.end());
+//         solve(0,c,t,st,combi);
+        
+//         vector<vector<int>> ans;
+//         for(auto &it : st){
+//             ans.push_back(it);
+//         }
+        
+//         return ans;
+//     }
+    
+    
+    
+    
+    
+    
+    void solve(int ind,int t,vector<int> &currCombi,vector<vector<int>> &ans,vector<int>&c){
+        if(t==0){
+            ans.push_back(currCombi);
+            return;
+        }
+        if(ind==c.size())return;
+        
+        for(int i=ind;i<c.size();++i){
+            if(c[i]>t)break;    // can't include any further elts in our current combination
+            if(i>ind && c[i]==c[i-1])continue;  // if current elt is same as previous one -> skip(already considered that case -> avoid duplicate combination)
+            
+            currCombi.push_back(c[i]);
+            solve(i+1,t-c[i],currCombi,ans,c);
+            currCombi.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& c, int t) {
+        vector<vector<int>> ans;
+        vector<int> currCombi;
+        sort(c.begin(),c.end());
+
+        solve(0,t,currCombi,ans,c);
+
+        return ans;
+    }
+};
