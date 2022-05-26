b0c2045b93b01b3959287ff04df1db69f126de70
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-05-26 16:27:35 +0530

    Added solution - LeetHub

---
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function template for C++
+
+class Solution{
+    public:
+    void solve(int i,int j,string move,vector<vector<int>>&vis,vector<string>&ans,vector<vector<int>> &dir,vector<vector<int>> &m,int n){
+        // BC
+        if(i==n-1 && j==n-1){
+            ans.push_back(move);
+            return;
+        }
+        
+        string dirName = "DLRU";
+        for(int it=0;it<4;++it){
+            int x = i + dir[it][0];
+            int y = j + dir[it][1];
+            
+            if(x>=0 && x<n && y>=0 && y<n && !vis[x][y] && m[x][y]==1){
+                vis[i][j] = 1;
+                solve(x,y,move+dirName[it],vis,ans,dir,m,n);
+                vis[i][j] = 0;
+            }
+        }
+    }
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        vector<string> ans;
+        vector<vector<int>> vis(n,vector<int>(n,0));
+        vector<vector<int>> dir = {{1,0},{0,-1},{0,1},{-1,0}};
+        
+        if(m[0][0]==1)solve(0,0,"",vis,ans,dir,m,n);   // if src = 0 then can't move rat from src to dest
+        
+        return ans;
+    }
+};
+
+    
+
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
