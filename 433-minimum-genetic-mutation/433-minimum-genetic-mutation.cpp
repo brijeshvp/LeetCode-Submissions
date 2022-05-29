46906d03ea14b319938492c86970b846546d67e2
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-05-29 16:40:27 +0530

    Time: 5 ms (14.52%), Space: 6.7 MB (11.41%) - LeetHub

--- 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int minMutation(string start, string end, vector<string>& bank) {
-        unordered_set<string> dict(bank.begin(), bank.end()),seen;
-        if(!dict.count(end)) return -1;
+        // dict keeps track of all possible valid genes and seen keeps track of already visited genes so that its not repeated
+        unordered_set<string> valids(bank.begin(), bank.end()),vis;
+        if(!valids.count(end)) return -1; // if dest not found in valid genes not possible to convert it to dest gene
+        // start(src) gene is assumed to be valid
         
         queue<string> q;
         q.push(start);
@@ -13,15 +15,14 @@ public:
                 string t = q.front();
                 q.pop();
                 if(t==end) return dist;
-                // addWord(t, dict, q);
                 
                 for(int i=0; i<t.size(); i++) {
                     char tmp = t[i];
                     for(char c:string("ACGT")) {
                         t[i] = c;
-                        if(dict.count(t) && !seen.count(t)) {
+                        if(valids.count(t) && !vis.count(t)) {
                             q.push(t);
-                            seen.insert(t);
+                            vis.insert(t);
                         }
                     }
                     t[i] = tmp;
@@ -32,19 +33,4 @@ public:
         return -1;
         
     }
-    
-    // void addWord(string word, unordered_set<string>& dict, queue<string>& q) {
-    //     dict.erase(word);
-    //     for(int i=0; i<word.size(); i++) {
-    //         char tmp = word[i];
-    //         for(char c:string("ACGT")) {
-    //             word[i] = c;
-    //             if(dict.count(word)) {
-    //                 q.push(word);
-    //                 dict.erase(word);
-    //             }
-    //         }
-    //         word[i] = tmp;
-    //     }
-    // }
 };
