4b57a8aa5c297cb47ebabbe3f3eb70712dc8723a
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-06-06 14:57:21 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (98.29%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -11,29 +11,52 @@
  */
 class Solution {
 public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> post;
+        
+// 		if(!root)return post;
+        
+// 		TreeNode* pre = NULL;
+//         stack<TreeNode*> st;
+// 		while(!st.empty() || root!=NULL){
+// 			if(root!=NULL){				
+// 				st.push(root);
+// 				root = root->left;
+// 			}
+// 			else{
+// 				root = st.top();
+// 				if(root->right==NULL || root->right==pre){
+// 				    post.push_back(root->val);
+// 				    st.pop();
+// 				    pre=root;
+// 				    root = NULL;
+// 				}
+// 				else root = root->right;
+// 			}
+// 		}
+// 		return post;
+//     }
+    
+    
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> post;
+        if(!root)return post;
         
-		if(!root)return post;
-        
-		TreeNode* pre = NULL;
+        // find root right left
+        // then take reverse
+        // it becomes left right root = postorder
         stack<TreeNode*> st;
-		while(!st.empty() || root!=NULL){
-			if(root!=NULL){				
-				st.push(root);
-				root = root->left;
-			}
-			else{
-				root = st.top();
-				if(root->right==NULL || root->right==pre){
-				    post.push_back(root->val);
-				    st.pop();
-				    pre=root;
-				    root = NULL;
-				}
-				else root = root->right;
-			}
-		}
-		return post;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* currNode = st.top();
+            st.pop();
+            
+            post.push_back(currNode->val);
+            if(currNode->left)st.push(currNode->left);
+            if(currNode->right)st.push(currNode->right);
+        }
+        
+        reverse(post.begin(),post.end());
+        return post;
     }
 };
