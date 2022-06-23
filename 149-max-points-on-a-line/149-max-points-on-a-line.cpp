e8c02983e733fd57ab971e447764072a026860c1
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-06-23 20:04:07 +0530

    Time: 100 ms (33.80%), Space: 17.3 MB (34.55%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -11,41 +11,7 @@ public:
         return ((double)p2[1]-p1[1])/((double)p2[0]-p1[0]);
     }
     
-    int maxPoints(vector<vector<int>>& p) {
-//         int n = p.size();
-//         if(n<=2)return n;
-        
-//         set<pair<int,int>> st;
-//         int mx = 1;
-//         for(int i=0;i<n && !st.count({p[i][0],p[i][1]});++i){
-//             auto it = p[i];
-            
-//             int same = 0;
-//             int localMx = 1;
-//             unordered_map<double,int> mp;
-            
-//             for(int j=i+1;j<n;++j){
-//                 if(areTwoPointsSame(it,p[j])){
-//                     same++;
-//                     continue;
-//                 }
-                
-//                 double slp = slope(it,p[j]);
-                
-//                 if(!mp.count(slp))mp[slp]+=2;   // to add both ith and jth point
-//                 else mp[slp]++;
-//                 localMx = max(localMx,mp[slp]);
-//             }
-            
-//             st.insert({it[0],it[1]});
-//             mx = max(mx,localMx + same);
-//         }
-//         return mx;
-        
-        
-        
-        
-        
+    int maxPoints(vector<vector<int>>& p) {        
         int n = p.size();
         if(n<=2)return n;
 
