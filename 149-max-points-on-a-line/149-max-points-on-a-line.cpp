c09baae864d7dd92abda736a232a2ce87954e9e3
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-06-23 20:03:41 +0530

    Time: 100 ms (33.80%), Space: 17.2 MB (34.55%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -12,34 +12,78 @@ public:
     }
     
     int maxPoints(vector<vector<int>>& p) {
+//         int n = p.size();
+//         if(n<=2)return n;
+        
+//         set<pair<int,int>> st;
+//         int mx = 1;
+//         for(int i=0;i<n && !st.count({p[i][0],p[i][1]});++i){
+//             auto it = p[i];
+            
+//             int same = 0;
+//             int localMx = 1;
+//             unordered_map<double,int> mp;
+            
+//             for(int j=i+1;j<n;++j){
+//                 if(areTwoPointsSame(it,p[j])){
+//                     same++;
+//                     continue;
+//                 }
+                
+//                 double slp = slope(it,p[j]);
+                
+//                 if(!mp.count(slp))mp[slp]+=2;   // to add both ith and jth point
+//                 else mp[slp]++;
+//                 localMx = max(localMx,mp[slp]);
+//             }
+            
+//             st.insert({it[0],it[1]});
+//             mx = max(mx,localMx + same);
+//         }
+//         return mx;
+        
+        
+        
+        
+        
         int n = p.size();
         if(n<=2)return n;
-        
-        set<pair<int,int>> st;
+
+        unordered_map<double, int> mp;
         int mx = 1;
-        for(int i=0;i<n && !st.count({p[i][0],p[i][1]});++i){
-            auto it = p[i];
-            
-            int same = 0;
-            int localMx = 1;
-            unordered_map<double,int> mp;
-            
-            for(int j=i+1;j<n;++j){
-                if(areTwoPointsSame(it,p[j])){
-                    same++;
-                    continue;
+        for(int i = 0; i < n; i++){
+            int duplicate = 1;
+            int vertical = 0;
+            for(int j = i+1; j < n; j++){
+                if(p[i][0] == p[j][0]){
+                    if(p[i][1] == p[j][1]){
+                        duplicate++;
+                    }
+                    else{
+                        vertical++;
+                    }
                 }
-                
-                double slp = slope(it,p[j]);
-                
-                if(!mp.count(slp))mp[slp]+=2;
-                else mp[slp]++;
-                localMx = max(localMx,mp[slp]);
+                else{
+                    vector<int> p1 = {p[i][0],p[i][1]};
+                    vector<int> p2 = {p[j][0],p[j][1]};
+                    double slp = slope(p1,p2);
+                    mp[slp]++;
+                }
             }
-            
-            st.insert({it[0],it[1]});
-            mx = max(mx,localMx + same);
+            auto it = mp.begin();
+            while(it != mp.end()){
+                int t = it->second;
+                if((t + duplicate) > mx){
+                    mx = t + duplicate;
+                }
+                it++;
+            }
+            if((vertical + duplicate) > mx){
+                mx = vertical + duplicate;
+            }
+            mp.clear(); // this is mandatory(think why!)
         }
+
         return mx;
     }
 };
