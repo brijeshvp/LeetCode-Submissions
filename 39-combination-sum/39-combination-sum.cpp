f3955491cbc0a0abc930d93e2f4b3f468f0f2936
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-10 14:49:18 +0530

    Time: 18 ms (46.07%), Space: 11.1 MB (67.30%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
-    void solve(int i,vector<int> &c,int t,vector<vector<int>> &ans,vector<int> &combi){
-        if(t<0)return;
-        if(t==0){
-            ans.push_back(combi);
-            // elts are +ve -> so going ahead will increase the sum -> thus return
-            return;
-        }
-        // if t!=0 but i == n
-        if(i==c.size())return;
+//     void solve(int i,vector<int> &c,int t,vector<vector<int>> &ans,vector<int> &combi){
+//         if(t<0)return;
+//         if(t==0){
+//             ans.push_back(combi);
+//             // elts are +ve -> so going ahead will increase the sum -> thus return
+//             return;
+//         }
+//         // if t!=0 but i == n
+//         if(i==c.size())return;
         
-        combi.push_back(c[i]);
-        solve(i,c,t-c[i],ans,combi);
-        combi.pop_back();   // backtrack
-        solve(i+1,c,t,ans,combi);
+//         combi.push_back(c[i]);
+//         solve(i,c,t-c[i],ans,combi);
+//         combi.pop_back();   // backtrack
+//         solve(i+1,c,t,ans,combi);
+//     }
+//     vector<vector<int>> combinationSum(vector<int>& c, int t) {
+//         vector<vector<int>> ans;
+//         vector<int> combi;
+//         solve(0,c,t,ans,combi);
+//         return ans;
+//     }
+    void solve(int ind,vector<int> &currCombination,vector<vector<int>> &allCombinations,vector<int> &a,int target){
+    int n = a.size();
+    // BCs
+    if(target<0)return;
+    if(target==0){
+        allCombinations.push_back(currCombination);
+        return;
     }
-    vector<vector<int>> combinationSum(vector<int>& c, int t) {
-        vector<vector<int>> ans;
-        vector<int> combi;
-        solve(0,c,t,ans,combi);
-        return ans;
-    }
+    if(ind==n)return;
+    
+    currCombination.push_back(a[ind]);
+    // target-=a[ind];
+    solve(ind,currCombination,allCombinations,a,target-a[ind]);
+    currCombination.pop_back();
+    // target+=a[ind];
+    solve(ind+1,currCombination,allCombinations,a,target);
+}
+
+vector<vector<int> > combinationSum(vector<int> &a, int b) {
+    // sort + remove duplicates
+    set<int> st(a.begin(),a.end());
+    a.assign(st.begin(),st.end());
+    
+    vector<vector<int>> allCombinations;
+    vector<int> currCombination;
+    solve(0,currCombination,allCombinations,a,b);
+    
+    sort(allCombinations.begin(),allCombinations.end());
+    
+    return allCombinations;
+}
 };
