8bb0ac78ad02d6fad628bdd8ae2434ce47fd5bf9
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-14 01:05:24 +0530

    Time: 21 ms (85.32%), Space: 19.6 MB (58.28%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,54 +1,13 @@
 class Solution {
 public:
-    // brute: for each elt, iterate in whole array to count number of times it occurs, if ct>n/2 return that elt
-    // TC: O(N^2) and SC: O(1)
-
-    
-    
-//     // intermidiate: TC: O(N) and SC: O(N)
-//     int majorityElement(vector<int>& a) {
-//         int n = a.size();
-        
-//         map<int,int> mp;
-//         for(int i=0;i<n;++i){
-//             mp[a[i]]++;
-//             if(mp[a[i]]>n/2)return a[i];
-//         }
-//         return -1;  // if no majority elt 
-//     }
-    
-    
-    
-    
-    
-    // optimal: TC: O(N) and SC: O(1)
-    int majorityElement(vector<int>& a) {
-//         int n = a.size();
-        
-//         int m = a[0];
-//         int ct = 1;
-//         for(int i=1;i<n;++i){
-//             if(a[i]==m)ct++;
-//             else ct--;
-            
-//             if(ct==0){
-//                 m = a[i];
-//                 ct++;
-//             }
-//         }
-        
-//         return m;
-        
-        int n = a.size();
+    int majorityElement(vector<int> &nums){
+        int candidate = INT_MIN;
         int votes = 0;
-        int candidate = -1;
-        for(int i=0;i<n;++i){
-            if(votes==0){
-                candidate = a[i];
-            }
+        for(int i : nums){
+            if(votes==0)candidate = i;
             
-            if(a[i]==candidate)votes++;
-            else votes--;
+            if(candidate != i)votes--;
+            else votes++;      
         }
         return candidate;
     }
