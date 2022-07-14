7e64d977d3d7224b1e1956604bee8f65b896a2cf
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-15 00:31:05 +0530

    Time: 1068 ms (40.36%), Space: 144.1 MB (50.44%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -13,13 +13,30 @@ private:
                 l++;
             }
         }
-       // worst case might be nlog(n) 
-        sort(a.begin() + lo, a.begin() + hi + 1);
-        return;
         
+        int lsz = mid - lo + 1;
+        int rsz = hi - mid;
+        vector<int> left(lsz,0);
+        vector<int> right(rsz,0);
+        for(int i=lo;i<=mid;++i)left[i-lo] = a[i];
+        for(int i=mid+1;i<=hi;++i)right[i-(mid+1)] = a[i];
         
-        
-        //every step sort
+        l = 0, r = 0;
+        int k = lo;
+        while(l < lsz && r < rsz){
+            if(left[l]<=right[r])a[k++] = left[l++];
+            else a[k++] = right[r++];
+        }
+        while(l<lsz){
+            a[k++] = left[l++];
+        }
+        while(r<rsz){
+            a[k++] = right[r++];
+        }
+       // // worst case might be nlog(n) 
+       //  sort(a.begin() + lo, a.begin() + hi + 1);
+       //  return;
+       //  //every step sort
     }
     void mergeSort(vector<int>& a, int lo, int hi){
         if(lo == hi) return;
