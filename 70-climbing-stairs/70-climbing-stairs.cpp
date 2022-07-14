f8ff05d9ccd414e99593c7cbbf3d00c7c6db32c3
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-14 16:08:32 +0530

    Time: 3 ms (35.83%), Space: 5.8 MB (83.64%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -4,16 +4,33 @@ public:
     // recurrence relation: f(i) = f(i+1) + f(i+2)
     // #ways to reach top from i = #ways to reach top from i+1 + #ways to reach top from i+2.
     // BC: dp[n] = 1, dp[n-1] = 2
+    
+//     // bottom-up
+//     int climbStairs(int n) {
+//         // BC
+//         if(n<=1)return n;
+        
+//         vector<int> dp(n+1,0);
+//         dp[n] = 1;
+//         dp[n-1] = 2;
+//         for(int i=n-2;i>=1;--i){
+//             dp[i] = dp[i+1] + dp[i+2];
+//         }
+//         return dp[1];
+//     }
+    
+    // space optimized bottom-up
     int climbStairs(int n) {
         // BC
         if(n<=1)return n;
         
-        vector<int> dp(n+1,0);
-        dp[n] = 1;
-        dp[n-1] = 2;
+        int prev2 = 1;
+        int prev1 = 2;
         for(int i=n-2;i>=1;--i){
-            dp[i] = dp[i+1] + dp[i+2];
+            int curr = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[1];
+        return prev1;
     }
 };
