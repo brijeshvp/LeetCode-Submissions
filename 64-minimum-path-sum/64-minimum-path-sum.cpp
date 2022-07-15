14421e40f7467d261fe6303fd206020985e673f5
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-16 01:36:48 +0530

    Time: 20 ms (25.88%), Space: 10.2 MB (28.77%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -3,7 +3,7 @@ public:
     // variation of unique path grid problems(refer dp sheet)
     int solve(int i,int j,vector<vector<int>>& grid,vector<vector<int>> &dp){
         int m = grid.size(), n = grid[0].size();
-        if(i>=m || j>=n)return INT_MAX;
+        if(i>=m || j>=n)return INT_MAX; // this is imp -> return INT_MAX bcoz we want to minimize the sum -> if we return 0, ans will be always 0(when it goes to invalid index)
         if(i==m-1 && j==n-1)return grid[i][j];
         if(dp[i][j]!=-1)return dp[i][j];
         
