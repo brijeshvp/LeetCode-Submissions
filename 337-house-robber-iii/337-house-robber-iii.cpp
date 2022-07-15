d1cee45ee923bec8fa421a20f3e612d4b142b050
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-15 11:21:45 +0530

    Time: 38 ms (26.08%), Space: 24.8 MB (47.20%) - LeetHub

---
+++ 337-house-robber-iii/337-house-robber-iii.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root,map<TreeNode*,int> &dp){
+        if(root==NULL)return 0;
+        if(dp.count(root))return dp[root];
+        
+        // choice 1
+        int sum1 = root->val;
+        if(root->left!=NULL){
+            sum1+=dfs(root->left->left,dp);
+            sum1+=dfs(root->left->right,dp);
+        }
+        if(root->right!=NULL){
+            sum1+=dfs(root->right->left,dp);
+            sum1+=dfs(root->right->right,dp);
+        }
+        //choice 2
+        int sum2= dfs(root->left,dp) + dfs(root->right,dp);
+        
+        return dp[root] = max(sum1,sum2);
+    }
+    
+    int rob(TreeNode* root) {
+        map<TreeNode*,int> dp;
+        return dfs(root,dp);
+    }
+};
