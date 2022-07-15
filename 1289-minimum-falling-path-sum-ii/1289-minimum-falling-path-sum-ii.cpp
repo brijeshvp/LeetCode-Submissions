b34480fd6f851251cab5df88a5a01043a60c2d7c
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-16 03:51:21 +0530

    Time: 623 ms (52.91%), Space: 15.7 MB (57.33%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // variable start, variable end question(variation of fixed start, variable end question -> triangle)
+    // also extention of medium question min falling path sum 
+    int solve(int i,vector<vector<int>>& matrix,vector<vector<int>> &dp,int lastCol){
+        int n = matrix.size();
+        if(i<0 || i>=n)return INT_MAX;   // return INT_MAX, since want to minimize sum
+        
+        int mn = INT_MAX,minCol=0;
+        for(int col=0;col<n;++col){
+            if(col==lastCol)continue;
+            if(dp[i][col]!=-1){
+                if(dp[i][col] < mn){
+                    mn = dp[i][col];
+                    minCol = col;
+                }
+                continue;
+            }
+            
+            int sum = matrix[i][col];
+            int ans = solve(i+1,matrix,dp,col);
+            if(ans!=INT_MAX)sum+=ans;
+            if(sum < mn){
+                mn = sum;
+                minCol = col;
+            }
+            dp[i][col] = sum;
+        }
+        return dp[i][minCol] = mn;
+    }
+    
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return solve(0,grid,dp,-1);
+    }
+};
