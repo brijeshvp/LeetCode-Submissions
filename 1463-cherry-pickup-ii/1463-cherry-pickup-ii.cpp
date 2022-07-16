c59855507cd94ed986fea91226a46b69802934a5
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-16 12:33:13 +0530

    Time: 367 ms (7.10%), Space: 9.3 MB (92.97%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -42,16 +42,59 @@ public:
     
     
     
-    // bottom-up
+//     // bottom-up
+//     // TC: O(m*n*n) and SC: O(m*n*n)
+//     int cherryPickup(vector<vector<int>>& grid) {
+//         int m = grid.size(), n = grid[0].size();
+//         vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n,0)));
+        
+//         // BC
+//         for(int j1=0;j1<n;++j1){
+//             for(int j2=0;j2<n;++j2){
+//                 if(j1==j2)dp[m-1][j1][j2] = grid[m-1][j1];
+//                 else dp[m-1][j1][j2] = grid[m-1][j1] + grid[m-1][j2];
+//             }
+//         }
+        
+//         for(int i=m-2;i>=0;--i){
+//             for(int j1=0;j1<n;++j1){
+//                 for(int j2=0;j2<n;++j2){      
+//                     int mx = 0;
+//                     for(int dj1=-1;dj1<=1;++dj1){
+//                         for(int dj2=-1;dj2<=1;++dj2){
+//                             int value = 0;
+//                             if(j1==j2)value = grid[i][j1];
+//                             else value = grid[i][j1] + grid[i][j2];
+//                             if(j1+dj1>=0 && j1+dj1<n && j2+dj2>=0 && j2+dj2<n)
+//                                 value+= dp[i+1][j1+dj1][j2+dj2];
+//                             else value = -1e8;
+//                             mx = max(mx,value);
+//                         }
+//                     }
+//                     dp[i][j1][j2] = mx;
+//                 }
+//             }
+//         }
+//         return dp[0][0][n-1];
+//     }
+    
+    
+    
+    
+    
+    
+    // bottom-up + space optimization
+    // TC: O(m*n*n) and SC: O(n*n)
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n,0)));
+        vector<vector<int>> next(n,vector<int>(n,0));
+        vector<vector<int>> curr(n,vector<int>(n,0));
         
         // BC
         for(int j1=0;j1<n;++j1){
             for(int j2=0;j2<n;++j2){
-                if(j1==j2)dp[m-1][j1][j2] = grid[m-1][j1];
-                else dp[m-1][j1][j2] = grid[m-1][j1] + grid[m-1][j2];
+                if(j1==j2)next[j1][j2] = grid[m-1][j1];
+                else next[j1][j2] = grid[m-1][j1] + grid[m-1][j2];
             }
         }
         
@@ -65,15 +108,16 @@ public:
                             if(j1==j2)value = grid[i][j1];
                             else value = grid[i][j1] + grid[i][j2];
                             if(j1+dj1>=0 && j1+dj1<n && j2+dj2>=0 && j2+dj2<n)
-                                value+= dp[i+1][j1+dj1][j2+dj2];
+                                value+= next[j1+dj1][j2+dj2];
                             else value = -1e8;
                             mx = max(mx,value);
                         }
                     }
-                    dp[i][j1][j2] = mx;
+                    curr[j1][j2] = mx;
                 }
             }
+            next = curr;
         }
-        return dp[0][0][n-1];
+        return next[0][n-1];
     }
 };
