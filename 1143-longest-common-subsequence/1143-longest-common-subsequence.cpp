ef5db44bf6d2bb1f12b8d62d7864f51d5248e4f1
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-18 18:26:21 +0530

    Time: 24 ms (71.63%), Space: 6.4 MB (96.10%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -21,21 +21,45 @@ public:
     
     
     
-    // bottom-up striver
+//     // bottom-up striver
+//     // TC: O(m*n) and SC: O(m*n)
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int m = text1.size(), n = text2.size();
+//         vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+//         // BC -> initialization
+//         for(int i=0;i<=m;++i)dp[i][0] = 0;
+//         for(int i=0;i<=n;++i)dp[0][i] = 0;
+        
+//         // recursion -> for loops
+//         for(int i=1;i<=m;++i){
+//             for(int j=1;j<=n;++j){
+//                 if(text1[i-1]==text2[j-1])dp[i][j] = 1 + dp[i-1][j-1];
+//                 else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+//             }
+//         }
+//         return dp[m][n];
+//     }
+    
+    
+    
+    
+    // bottom-up + space-optimization striver
+    // TC: O(m*n) and SC: O(2n)
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size(), n = text2.size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        vector<int> prev(n+1,0),curr(n+1,0);
         // BC -> initialization
-        for(int i=0;i<=m;++i)dp[i][0] = 0;
-        for(int i=0;i<=n;++i)dp[0][i] = 0;
+        // for(int i=0;i<=m;++i)dp[i][0] = 0;
+        for(int i=0;i<=n;++i)prev[i] = 0;
         
         // recursion -> for loops
         for(int i=1;i<=m;++i){
             for(int j=1;j<=n;++j){
-                if(text1[i-1]==text2[j-1])dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                if(text1[i-1]==text2[j-1])curr[j] = 1 + prev[j-1];
+                else curr[j] = max(prev[j],curr[j-1]);
             }
+            prev = curr;
         }
-        return dp[m][n];
+        return prev[n];
     }
 };
