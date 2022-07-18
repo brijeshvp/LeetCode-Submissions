832b57423b2e78a10b2445c034d855fe3341fd65
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-18 18:20:50 +0530

    Time: 56 ms (18.51%), Space: 13.3 MB (10.98%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    // top-down striver
+    // TC: O(m*n) and SC: O(m*n + m+n) -> dp array + call stack space
+    int solve(int m,int n,string &a,string &b,vector<vector<int>> &dp){
+        // BC
+        if(m==0 || n==0)return 0;
+        if(dp[m][n]!=-1)return dp[m][n];
+        
+        if(a[m-1]==b[n-1])return dp[m][n] = 1 + solve(m-1,n-1,a,b,dp);
+        else return dp[m][n] = max(solve(m-1,n,a,b,dp),solve(m,n-1,a,b,dp));
+    }
+    
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size(), n = text2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return solve(m,n,text1,text2,dp);
+    }
+};
