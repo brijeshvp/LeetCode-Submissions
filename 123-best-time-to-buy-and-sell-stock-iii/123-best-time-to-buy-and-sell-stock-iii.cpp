a16d7ee8f311de591ba6f7e5db11452c41adf4f3
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-19 19:41:54 +0530

    Time: 295 ms (56.09%), Space: 75.3 MB (85.13%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -33,41 +33,86 @@ public:
     
     
     
-    // bottom-up striver
+//     // bottom-up striver
+//     // TC: O(n*2*3) and SC: O(n*2*3)
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+//         // BC-1
+//         for(int i=0;i<=1;++i){
+//             for(int j=0;j<=2;++j){
+//                 dp[n][i][j] = 0;
+//             }
+//         }
+//         // BC-2
+//         for(int i=0;i<n;++i){
+//             for(int j=0;j<=1;++j){
+//                 dp[i][j][0] = 0;
+//             }
+//         }
+        
+//         for(int i=n-1;i>=0;--i){
+//             for(int j=0;j<=1;++j){
+//                 for(int k=1;k<=2;++k){  // k(capacity) = 0 -> BC
+//                     int profit = 0;
+//                     if(j){
+//                         int buy = -prices[i] + dp[i+1][0][k];
+//                         int notbuy = 0 + dp[i+1][1][k];
+//                         profit = max(buy,notbuy);
+//                     }
+//                     else{
+//                         int sell = prices[i] + dp[i+1][1][k-1];
+//                         int notsell = 0 + dp[i+1][0][k];
+//                         profit = max(sell,notsell);
+//                     }
+//                     dp[i][j][k] = profit;
+//                 }
+//             }
+//         }
+//         return dp[0][1][2];
+//     }
+    
+    
+    
+    
+    
+    // bottom-up + space-optimization striver
+    // TC: O(n*2*3) and SC: O(n*2*3)
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        // BC-1
-        for(int i=0;i<=1;++i){
-            for(int j=0;j<=2;++j){
-                dp[n][i][j] = 0;
-            }
-        }
-        // BC-2
-        for(int i=0;i<n;++i){
-            for(int j=0;j<=1;++j){
-                dp[i][j][0] = 0;
-            }
-        }
+        vector<vector<int>> next(2,vector<int>(3,0)), curr(2,vector<int>(3,0));
+        // // BC-1
+        // for(int i=0;i<=1;++i){
+        //     for(int j=0;j<=2;++j){
+        //         dp[n][i][j] = 0;
+        //     }
+        // }
+        // // BC-2
+        // for(int i=0;i<n;++i){
+        //     for(int j=0;j<=1;++j){
+        //         dp[i][j][0] = 0;
+        //     }
+        // }
         
         for(int i=n-1;i>=0;--i){
             for(int j=0;j<=1;++j){
                 for(int k=1;k<=2;++k){  // k(capacity) = 0 -> BC
                     int profit = 0;
                     if(j){
-                        int buy = -prices[i] + dp[i+1][0][k];
-                        int notbuy = 0 + dp[i+1][1][k];
+                        int buy = -prices[i] + next[0][k];
+                        int notbuy = 0 + next[1][k];
                         profit = max(buy,notbuy);
                     }
                     else{
-                        int sell = prices[i] + dp[i+1][1][k-1];
-                        int notsell = 0 + dp[i+1][0][k];
+                        int sell = prices[i] + next[1][k-1];
+                        int notsell = 0 + next[0][k];
                         profit = max(sell,notsell);
                     }
-                    dp[i][j][k] = profit;
+                    curr[j][k] = profit;
                 }
+                next = curr;
             }
         }
-        return dp[0][1][2];
+        return next[1][2];
     }
 };
