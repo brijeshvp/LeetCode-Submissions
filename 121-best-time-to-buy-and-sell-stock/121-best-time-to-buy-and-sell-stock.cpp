2797890a9eb4b72edc7d30133c83e42687390ba2
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-19 15:45:25 +0530

    Time: 208 ms (43.72%), Space: 97.3 MB (5.42%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     int maxProfit(vector<int>& p) {
+        int n = p.size();
+        
+        vector<int> prefMin(n,0);
+        prefMin[0] = p[0];
+        
+        int mx = 0;
+        for(int i=1;i<n;++i){
+            prefMin[i] = min(prefMin[i-1],p[i]);
+            mx = max(mx,p[i]-prefMin[i]);
+        }
+        
+        // int mx = 0;
+        // for(int i=0;i<n;++i){
+        //     mx = max(mx,p[i]-prefMin[i]);
+        // }
+        
+        return mx;
+        
+        
 //         int n = p.size();
         
-//         vector<int> prefMin(n,0);
-//         prefMin[0] = p[0];
-        
-//         for(int i=1;i<n;++i){
-//             prefMin[i] = min(prefMin[i-1],p[i]);
-//         }
-        
+//         int prefMin = INT_MAX;
 //         int mx = 0;
 //         for(int i=0;i<n;++i){
-//             mx = max(mx,p[i]-prefMin[i]);
+//             prefMin = min(prefMin,p[i]);
+//             mx = max(mx,p[i]-prefMin);
 //         }
         
 //         return mx;
-        
-        
-        int n = p.size();
-        
-        int prefMin = INT_MAX;
-        int mx = 0;
-        for(int i=0;i<n;++i){
-            prefMin = min(prefMin,p[i]);
-            mx = max(mx,p[i]-prefMin);
-        }
-        
-        return mx;
     }
 };
