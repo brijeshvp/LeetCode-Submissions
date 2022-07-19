db3699c27e9ba9d169716848aa657e79bbdae010
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-19 21:32:12 +0530

    Time: 3 ms (91.22%), Space: 11.1 MB (89.85%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -29,27 +29,59 @@ public:
     
     
     
-    // bottom-up striver
+//     // bottom-up striver
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<int>> dp(n+2,vector<int>(2,0));   // n+2 size vector bcoz of ind+2 in sell case
+        
+//         for(int ind=n-1;ind>=0;--ind){
+//             for(int canBuy=0;canBuy<=1;++canBuy){
+//                 int profit = 0;
+//                 if(canBuy){
+//                     int buy = -prices[ind] + dp[ind+1][0];
+//                     int notbuy = 0 + dp[ind+1][1];
+//                     profit = max(buy,notbuy);
+//                 }
+//                 else{
+//                     int sell = prices[ind] + dp[ind+2][1];
+//                     int notsell = 0 + dp[ind+1][0];
+//                     profit = max(sell,notsell);
+//                 }
+//                 dp[ind][canBuy] = profit;
+//             }
+//         }
+//         return dp[0][1];    // at 0th ind -> we can always buy -> 2nd parameter = 1, also we run from n-1 to 0 -> so at last we return dp[0][1]
+//     }
+    
+    
+    
+    
+    
+    
+    // bottom-up + space-optimization-1 striver
+    // TC: O(2n) and SC: O(4)
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+2,vector<int>(2,0));   // n+2 size vector bcoz of ind+2 in sell case
+        vector<int> next2(2,0), next1(2,0), curr(2,0);
         
         for(int ind=n-1;ind>=0;--ind){
             for(int canBuy=0;canBuy<=1;++canBuy){
                 int profit = 0;
                 if(canBuy){
-                    int buy = -prices[ind] + dp[ind+1][0];
-                    int notbuy = 0 + dp[ind+1][1];
+                    int buy = -prices[ind] + next1[0];
+                    int notbuy = 0 + next1[1];
                     profit = max(buy,notbuy);
                 }
                 else{
-                    int sell = prices[ind] + dp[ind+2][1];
-                    int notsell = 0 + dp[ind+1][0];
+                    int sell = prices[ind] + next2[1];
+                    int notsell = 0 + next1[0];
                     profit = max(sell,notsell);
                 }
-                dp[ind][canBuy] = profit;
+                curr[canBuy] = profit;
             }
+            next2 = next1;
+            next1 = curr;
         }
-        return dp[0][1];    // at 0th ind -> we can always buy -> 2nd parameter = 1, also we run from n-1 to 0 -> so at last we return dp[0][1]
+        return next1[1];    // at 0th ind -> we can always buy -> 2nd parameter = 1, also we run from n-1 to 0 -> so at last we return dp[0][1]
     }
 };
