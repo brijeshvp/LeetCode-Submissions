3f8fba08314d12a9471acf5a245c2c39b6c5bbd7
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-19 20:47:04 +0530

    Time: 567 ms (37.87%), Space: 135.3 MB (35.97%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -122,30 +122,59 @@ public:
     
     
     // method-2 2d DP striver
-    // top-down striver
-    // TC: O(n*4) and SC: O(n*4) + O(n) -> dp array + call stack
-    int solve(int ind,int transactions,vector<int> &prices,vector<vector<int>> &dp){
-        int n = prices.size();
-        if(ind==n || transactions==4)return 0;
-        if(dp[ind][transactions]!=-1)return dp[ind][transactions];
+//     // top-down striver
+//     // TC: O(n*4) and SC: O(n*4) + O(n) -> dp array + call stack
+//     int solve(int ind,int transactions,vector<int> &prices,vector<vector<int>> &dp){
+//         int n = prices.size();
+//         if(ind==n || transactions==4)return 0;
+//         if(dp[ind][transactions]!=-1)return dp[ind][transactions];
         
-        int profit = 0;
-        if(transactions%2==0){
-            int buy = -prices[ind] + solve(ind+1,transactions+1,prices,dp);
-            int notbuy = 0 + solve(ind+1,transactions,prices,dp);
-            profit = max(buy,notbuy);
-        }
-        else{
-            int sell = prices[ind] + solve(ind+1,transactions+1,prices,dp);
-            int notsell = 0 + solve(ind+1,transactions,prices,dp);
-            profit = max(sell,notsell);
-        }
-        return dp[ind][transactions] = profit;
-    }
+//         int profit = 0;
+//         if(transactions%2==0){
+//             int buy = -prices[ind] + solve(ind+1,transactions+1,prices,dp);
+//             int notbuy = 0 + solve(ind+1,transactions,prices,dp);
+//             profit = max(buy,notbuy);
+//         }
+//         else{
+//             int sell = prices[ind] + solve(ind+1,transactions+1,prices,dp);
+//             int notsell = 0 + solve(ind+1,transactions,prices,dp);
+//             profit = max(sell,notsell);
+//         }
+//         return dp[ind][transactions] = profit;
+//     }
     
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<int>> dp(n,vector<int>(4,-1));
+//         return solve(0,0,prices,dp);
+//     }
+    
+    
+    
+    
+    
+    // bottom-up striver
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(4,-1));
-        return solve(0,0,prices,dp);
+        int k = 2;
+        vector<vector<int>> dp(n+1,vector<int>(2*k+1,0));
+        
+        for(int ind=n-1;ind>=0;--ind){
+            for(int tranNo=2*k-1;tranNo>=0;--tranNo){
+                int profit = 0;
+                if(tranNo%2==0){
+                    int buy = -prices[ind] + dp[ind+1][tranNo+1];
+                    int notbuy = 0 + dp[ind+1][tranNo];
+                    profit = max(buy,notbuy);
+                }
+                else{
+                    int sell = prices[ind] + dp[ind+1][tranNo+1];
+                    int notsell = 0 + dp[ind+1][tranNo];
+                    profit = max(sell,notsell);
+                }
+                dp[ind][tranNo] = profit;
+            }
+        }
+        return dp[0][0];
     }
 };
