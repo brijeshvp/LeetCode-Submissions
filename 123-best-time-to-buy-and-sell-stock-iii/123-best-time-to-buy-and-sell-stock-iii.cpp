252cdc0564ee47d290ae68bf282c1b1810d4f1dc
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-19 19:46:16 +0530

    Time: 628 ms (36.57%), Space: 132.6 MB (40.37%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // method-1 : 3d dp 
+    // method-1 : 3d dp striver
     
 //     // top-down striver
 //     // TC: O(n*2*3) and SC: O(n*2*3) + O(n) -> dp array + call stack
@@ -76,43 +76,76 @@ public:
     
     
     
-    // bottom-up + space-optimization striver
-    // TC: O(n*2*3) and SC: O(n*2*3)
+//     // bottom-up + space-optimization striver
+//     // TC: O(n*2*3) and SC: O(2*3)
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<int>> next(2,vector<int>(3,0)), curr(2,vector<int>(3,0));
+//         // // BC-1
+//         // for(int i=0;i<=1;++i){
+//         //     for(int j=0;j<=2;++j){
+//         //         dp[n][i][j] = 0;
+//         //     }
+//         // }
+//         // // BC-2
+//         // for(int i=0;i<n;++i){
+//         //     for(int j=0;j<=1;++j){
+//         //         dp[i][j][0] = 0;
+//         //     }
+//         // }
+        
+//         for(int i=n-1;i>=0;--i){
+//             for(int j=0;j<=1;++j){
+//                 for(int k=1;k<=2;++k){  // k(capacity) = 0 -> BC
+//                     int profit = 0;
+//                     if(j){
+//                         int buy = -prices[i] + next[0][k];
+//                         int notbuy = 0 + next[1][k];
+//                         profit = max(buy,notbuy);
+//                     }
+//                     else{
+//                         int sell = prices[i] + next[1][k-1];
+//                         int notsell = 0 + next[0][k];
+//                         profit = max(sell,notsell);
+//                     }
+//                     curr[j][k] = profit;
+//                 }
+//                 next = curr;
+//             }
+//         }
+//         return next[1][2];
+//     }
+    
+    
+    
+    
+    
+    
+    // method-2 2d DP striver
+    // top-down striver
+    // TC: O(n*2*3) and SC: O(n*2*3) + O(n) -> dp array + call stack
+    int solve(int ind,int transactions,vector<int> &prices,vector<vector<int>> &dp){
+        int n = prices.size();
+        if(ind==n || transactions==4)return 0;
+        if(dp[ind][transactions]!=-1)return dp[ind][transactions];
+        
+        int profit = 0;
+        if(transactions%2==0){
+            int buy = -prices[ind] + solve(ind+1,transactions+1,prices,dp);
+            int notbuy = 0 + solve(ind+1,transactions,prices,dp);
+            profit = max(buy,notbuy);
+        }
+        else{
+            int sell = prices[ind] + solve(ind+1,transactions+1,prices,dp);
+            int notsell = 0 + solve(ind+1,transactions,prices,dp);
+            profit = max(sell,notsell);
+        }
+        return dp[ind][transactions] = profit;
+    }
+    
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> next(2,vector<int>(3,0)), curr(2,vector<int>(3,0));
-        // // BC-1
-        // for(int i=0;i<=1;++i){
-        //     for(int j=0;j<=2;++j){
-        //         dp[n][i][j] = 0;
-        //     }
-        // }
-        // // BC-2
-        // for(int i=0;i<n;++i){
-        //     for(int j=0;j<=1;++j){
-        //         dp[i][j][0] = 0;
-        //     }
-        // }
-        
-        for(int i=n-1;i>=0;--i){
-            for(int j=0;j<=1;++j){
-                for(int k=1;k<=2;++k){  // k(capacity) = 0 -> BC
-                    int profit = 0;
-                    if(j){
-                        int buy = -prices[i] + next[0][k];
-                        int notbuy = 0 + next[1][k];
-                        profit = max(buy,notbuy);
-                    }
-                    else{
-                        int sell = prices[i] + next[1][k-1];
-                        int notsell = 0 + next[0][k];
-                        profit = max(sell,notsell);
-                    }
-                    curr[j][k] = profit;
-                }
-                next = curr;
-            }
-        }
-        return next[1][2];
+        vector<vector<int>> dp(n,vector<int>(4,-1));
+        return solve(0,0,prices,dp);
     }
 };
