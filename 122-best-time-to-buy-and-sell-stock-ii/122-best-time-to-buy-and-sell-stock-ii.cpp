d2777707339a22566fcc3705f29618048bf746f2
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-19 18:37:11 +0530

    Time: 37 ms (5.11%), Space: 15.2 MB (15.22%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,28 +1,59 @@
 class Solution {
 public:
-    // top-down striver
-    int solve(int ind,int canBuy,vector<int> &prices,vector<vector<int>> &dp){
-        int n = prices.size();
-        if(ind==n)return 0;
-        if(dp[ind][canBuy]!=-1)return dp[ind][canBuy];
+//     // top-down striver
+//     int solve(int ind,int canBuy,vector<int> &prices,vector<vector<int>> &dp){
+//         int n = prices.size();
+//         if(ind==n)return 0;
+//         if(dp[ind][canBuy]!=-1)return dp[ind][canBuy];
         
-        int profit = 0;
-        if(canBuy){
-            int buy = -prices[ind] + solve(ind+1,0,prices,dp);
-            int notbuy = 0 + solve(ind+1,1,prices,dp);
-            profit = max(buy,notbuy);
-        }
-        else{
-            int sell = prices[ind] + solve(ind+1,1,prices,dp);
-            int notsell = 0 + solve(ind+1,0,prices,dp);
-            profit = max(sell,notsell);
-        }
-        return dp[ind][canBuy] = profit;
-    }
+//         int profit = 0;
+//         if(canBuy){
+//             int buy = -prices[ind] + solve(ind+1,0,prices,dp);
+//             int notbuy = 0 + solve(ind+1,1,prices,dp);
+//             profit = max(buy,notbuy);
+//         }
+//         else{
+//             int sell = prices[ind] + solve(ind+1,1,prices,dp);
+//             int notsell = 0 + solve(ind+1,0,prices,dp);
+//             profit = max(sell,notsell);
+//         }
+//         return dp[ind][canBuy] = profit;
+//     }
     
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<int>> dp(n,vector<int>(2,-1));
+//         return solve(0,1,prices,dp);
+//     }
+    
+    
+    
+    
+    
+    
+    // bottom-up striver
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return solve(0,1,prices,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        
+        dp[n][0] = dp[n][1] = 0;
+        
+        for(int ind=n-1;ind>=0;--ind){
+            for(int canBuy=0;canBuy<=1;++canBuy){
+                int profit = 0;
+                if(canBuy){
+                    int buy = -prices[ind] + dp[ind+1][0];
+                    int notbuy = 0 + dp[ind+1][1];
+                    profit = max(buy,notbuy);
+                }
+                else{
+                    int sell = prices[ind] + dp[ind+1][1];
+                    int notsell = 0 + dp[ind+1][0];
+                    profit = max(sell,notsell);
+                }
+                dp[ind][canBuy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };
