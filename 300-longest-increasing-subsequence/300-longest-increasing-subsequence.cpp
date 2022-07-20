5fddeabdb7f506aea8a298002ef1d854dc80f1af
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-20 12:23:05 +0530

    Time: 2338 ms (5.00%), Space: 292.3 MB (6.79%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
-    // top-down striver
-    int solve(int ind,int prev,vector<int> &a,vector<vector<int>> &dp){
-        int n = a.size();
-        if(ind==n)return 0;
-        if(dp[ind][prev+1]!=-1)return dp[ind][prev+1];
-        
-        int nottake = 0 + solve(ind+1,prev,a,dp);
-        int take = 0;
-        if(prev==-1 || a[ind]>a[prev])take = 1 + solve(ind+1,ind,a,dp);
-        return dp[ind][prev+1] = max(take,nottake);
-    }
-    
-    int lengthOfLIS(vector<int>& a) {
+    // method-1 -> 2D DP
+//     // top-down striver
+//     // TC: O(n*n) and SC: O(n*n) + O(n) -> dp array + call stack
+//     int solve(int ind,int prev,vector<int> &a,vector<vector<int>> &dp){
 //         int n = a.size();
+//         if(ind==n)return 0;
+//         if(dp[ind][prev+1]!=-1)return dp[ind][prev+1];
         
-//         vector<int> LIS(n+1,1);
-        
-//         for(int i=1;i<n;++i){
-//             for(int j=0;j<i;++j){
-//                 if(a[j]<a[i] && LIS[i]<1+LIS[j])LIS[i] = 1 + LIS[j];
-//             }
-//         }
-        
-//         int mx = 0;
-//         for(int i:LIS)mx = max(mx,i);
-//         return mx;
-        
+//         int nottake = 0 + solve(ind+1,prev,a,dp);
+//         int take = 0;
+//         if(prev==-1 || a[ind]>a[prev])take = 1 + solve(ind+1,ind,a,dp);
+//         return dp[ind][prev+1] = max(take,nottake);
+//     }
+    
+//     int lengthOfLIS(vector<int>& a) {        
+//         int n = a.size();
+//         vector<vector<int>> dp(n,vector<int>(n+1,-1));
+//         return solve(0,-1,a,dp);
+//     }
+    
+    
+    
+    // bottom-up striver
+    int lengthOfLIS(vector<int>& a) {        
         int n = a.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return solve(0,-1,a,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        for(int ind=n-1;ind>=0;--ind){
+            for(int prev=n-1;prev>=-1;--prev){
+                int nottake = 0 + dp[ind+1][prev+1];
+                int take = 0;
+                if(prev==-1 || a[ind]>a[prev])take = 1 + dp[ind+1][ind+1];
+                dp[ind][prev+1] = max(take,nottake);
+            }
+        }
+        return dp[0][0];
     }
 };
