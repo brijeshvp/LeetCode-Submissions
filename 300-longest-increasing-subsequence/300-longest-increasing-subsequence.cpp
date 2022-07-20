721462283914a34a98153ac9382264398b740f07
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-20 12:09:56 +0530

    Time: 2519 ms (5.00%), Space: 292.3 MB (6.79%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& a) {
+    // top-down striver
+    int solve(int ind,int prev,vector<int> &a,vector<vector<int>> &dp){
         int n = a.size();
+        if(ind==n)return 0;
+        if(dp[ind][prev+1]!=-1)return dp[ind][prev+1];
         
-        vector<int> LIS(n+1,1);
+        int nottake = 0 + solve(ind+1,prev,a,dp);
+        int take = 0;
+        if(prev==-1 || a[ind]>a[prev])take = 1 + solve(ind+1,ind,a,dp);
+        return dp[ind][prev+1] = max(take,nottake);
+    }
+    
+    int lengthOfLIS(vector<int>& a) {
+//         int n = a.size();
         
-        for(int i=1;i<n;++i){
-            for(int j=0;j<i;++j){
-                if(a[j]<a[i] && LIS[i]<1+LIS[j])LIS[i] = 1 + LIS[j];
-            }
-        }
+//         vector<int> LIS(n+1,1);
         
-        int mx = 0;
-        for(int i:LIS)mx = max(mx,i);
-        return mx;
+//         for(int i=1;i<n;++i){
+//             for(int j=0;j<i;++j){
+//                 if(a[j]<a[i] && LIS[i]<1+LIS[j])LIS[i] = 1 + LIS[j];
+//             }
+//         }
+        
+//         int mx = 0;
+//         for(int i:LIS)mx = max(mx,i);
+//         return mx;
+        
+        int n = a.size();
+        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        return solve(0,-1,a,dp);
     }
 };
