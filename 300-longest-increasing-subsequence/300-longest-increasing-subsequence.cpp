17040b47073f61b82495d30562ac14e5322feda7
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-20 12:24:29 +0530

    Time: 2240 ms (5.00%), Space: 292.1 MB (9.44%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -23,18 +23,21 @@ public:
     
     
     // bottom-up striver
+    // TC: O(n*n) and SC: O(n*n)
     int lengthOfLIS(vector<int>& a) {        
         int n = a.size();
         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
         
         for(int ind=n-1;ind>=0;--ind){
             for(int prev=n-1;prev>=-1;--prev){
-                int nottake = 0 + dp[ind+1][prev+1];
+                int nottake = 0 + dp[ind+1][prev+1];    // 2nd parameter -> prev+1 -> since prev can be -1
                 int take = 0;
-                if(prev==-1 || a[ind]>a[prev])take = 1 + dp[ind+1][ind+1];
-                dp[ind][prev+1] = max(take,nottake);
+                if(prev==-1 || a[ind]>a[prev])take = 1 + dp[ind+1][ind+1];  // 2nd parameter -> prev+1 -> since prev can be -1
+                dp[ind][prev+1] = max(take,nottake);    // 2nd parameter -> prev+1 -> since prev can be -1
             }
         }
-        return dp[0][0];
+        // 2nd parameter -> prev+1 -> since prev can be -1
+        // our final ans = dp[0][-1] -> so return dp[0][0] -> since prev = -1 -> shifted to prev = 0
+        return dp[0][0];    
     }
 };
