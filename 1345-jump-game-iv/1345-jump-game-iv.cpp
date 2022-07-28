7d6728380fe04d6e74888b395ed7b508e096ca75
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-07-28 14:37:42 +0530

    Time: 510 ms (26.23%), Space: 110.6 MB (15.16%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
-    int minJumps(vector<int>& arr) {
-        int n = arr.size();
-        unordered_map<int, vector<int>> indicesOfValue;
-        for (int i = 0; i < n; i++)
-            indicesOfValue[arr[i]].push_back(i);
-        vector<bool> visited(n); visited[0] = true;
-        queue<int> q; q.push(0);
-        int step = 0;
+    int minJumps(vector<int>& a) {
+        int n = a.size();
+        unordered_map<int, vector<int>> adj;
+        for (int i = 0; i < n; i++){
+            adj[a[i]].push_back(i);    
+        }
+            
+        set<int> vis; 
+        vis.insert(0);
+        queue<pair<int,int>> q; 
+        q.push({0,0});
+        
+        int steps = 0;
         while (!q.empty()) {
-            for (int size = q.size(); size > 0; --size) {
-                int i = q.front(); q.pop();
-                if (i == n - 1) return step; // Reached to last index
-                vector<int>& next = indicesOfValue[arr[i]];
-                next.push_back(i - 1); next.push_back(i + 1);
-                for (int j : next) {
-                    if (j >= 0 && j < n && !visited[j]) {
-                        visited[j] = true;
-                        q.push(j);
+            int sz = q.size();
+            while(sz--){
+                int ind = q.front().first;
+                int dist = q.front().second;
+                q.pop();
+                
+                if (ind == n - 1) return dist; // Reached to last index
+                
+                vector<int>& next = adj[a[ind]];
+                if(ind-1>=0)next.push_back(ind - 1); 
+                if(ind+1<n)next.push_back(ind + 1);
+                
+                for (auto it : next) {
+                    if (!vis.count(it)) {
+                        vis.insert(it);
+                        q.push({it,dist+1});
                     }
                 }
-                next.clear(); // avoid later lookup indicesOfValue arr[i]
+                next.clear(); // avoid later lookup adj a[ind]
             }
-            step++;
+            steps++;
         }
         return 0;
     }
