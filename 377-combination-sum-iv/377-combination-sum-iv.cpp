e8dcbb4d20a888721868ba4a2ed340ff3d8d1368
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-08-05 21:57:19 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -2,32 +2,21 @@
 // https://leetcode.com/problems/combination-sum-iv/discuss/85120/C%2B%2B-template-for-ALL-Combination-Problem-Set
 class Solution {
 public:
-    int count(vector<int> &nums, int target, int pos,vector<int> &dp) {
-        if (pos == nums.size() || target <= 0) {
-            return (target == 0) ? 1 : 0;
-        }
+    int solve(int i,vector<int> &nums, int target,vector<int> &dp) {
+        int n = nums.size();
+        if(target==0)return 1;
+        if(target<0 || i>=n)return 0;
 
-        if (dp[target] != -1) {
-            return dp[target];
-        }
+        if (dp[target] != -1) return dp[target];
 
-        int take = count(nums, target - nums[pos], 0, dp);
-        int skip = count(nums, target, pos + 1, dp);
+        int take = solve(0,nums, target - nums[i], dp);
+        int nontake = solve(i+1,nums, target, dp);
 
-        return dp[target] = take + skip;
+        return dp[target] = take + nontake;
     }
+    
     int combinationSum4(vector<int>& nums, int target) {
         vector<int> dp(target+1,-1);
-        return count(nums, target, 0, dp);
-        // vector<long long> dp(target+1);
-        // dp[0] = 1;
-        // for (int i = 1; i <= target; i++) {
-        //     for (int n : nums) {
-        //         if (i >= n) {
-        //             dp[i] += dp[i - n];
-        //         }
-        //     }
-        // }
-        // return dp[target];
+        return solve(0,nums,target,dp);
     }
 };
