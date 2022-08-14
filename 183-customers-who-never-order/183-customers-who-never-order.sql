ef23aaec93dc1061fa69e4e80a38cd97cb9bbfb5
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-08-14 20:15:31 +0530

    Time: 475 ms (72.76%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -3,4 +3,7 @@
 # select name as Customers from Customers  where id NOT IN (select customerId from Orders);\
 
 # correlated subquery
-SELECT A.Name as Customers from Customers A WHERE NOT EXISTS (SELECT 1 FROM Orders B WHERE A.Id = B.CustomerId)
+# SELECT A.Name as Customers from Customers A WHERE NOT EXISTS (SELECT 1 FROM Orders B WHERE A.Id = B.CustomerId)
+
+# using join
+SELECT A.Name as Customers from Customers A LEFT JOIN Orders B on  a.Id = B.CustomerId WHERE b.CustomerId is NULL
