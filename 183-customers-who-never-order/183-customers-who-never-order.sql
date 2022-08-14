14c91a7d47f412bdfd58b054d06a5eccb84ad73e
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-08-14 20:14:31 +0530

    Time: 506 ms (64.78%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
-select name as Customers from Customers  where id NOT IN (select customerId from Orders);
+# nested query
+# select name as Customers from Customers  where id NOT IN (select customerId from Orders);\
+
+# correlated subquery
+SELECT A.Name as Customers from Customers A WHERE NOT EXISTS (SELECT 1 FROM Orders B WHERE A.Id = B.CustomerId)
