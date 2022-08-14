01720cdf8e2b9469ec8cec971af9e181f97cadde
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-08-14 20:16:28 +0530

    Time: 389 ms (96.45%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -6,4 +6,4 @@
 # SELECT A.Name as Customers from Customers A WHERE NOT EXISTS (SELECT 1 FROM Orders B WHERE A.Id = B.CustomerId)
 
 # using join
-SELECT A.Name as Customers from Customers A LEFT JOIN Orders B on  a.Id = B.CustomerId WHERE b.CustomerId is NULL
+SELECT A.Name as Customers from Customers A LEFT JOIN Orders B on  A.Id = B.CustomerId WHERE B.CustomerId is NULL
