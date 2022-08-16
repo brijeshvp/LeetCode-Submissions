b6473464063feef92cdf6745502981b02c5453f9
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-08-16 11:03:53 +0530

    Time: 288 ms (54.08%), Space: 0B (100.00%) - LeetHub

--- 627-swap-salary/627-swap-salary.sql
+++ 627-swap-salary/627-swap-salary.sql
@@ -3,4 +3,12 @@
 # select statement will not work bcoz we need to update in same table itself(we just don't want output)
 # select id,name, if(sex='f','m','f') as sex,salary from Salary;
 
-update Salary set sex=if(sex='m','f','m');
+# WORKING QUERIES
+# update Salary set sex=if(sex='m','f','m');
+update Salary set sex=(
+CASE
+    WHEN sex='m' THEN 'f'
+    ELSE 'm'
+END
+)
+# update Salary set (sex='m' where sex='f') and (sex='f' where sex='m');
