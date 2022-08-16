c2cfe02cea1a151e43aec0ce7d312fbdecaaff3d
Brijesh Peshvani <57172793+brijeshvp@users.noreply.github.com>
2022-08-16 10:48:30 +0530

    Time: 650 ms (59.01%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -2,3 +2,6 @@
 
 # if(condition, value_if_true, value_if_false) this is the syntax of if in mysql
 SELECT employee_id, IF(employee_id%2=1 AND name NOT LIKE "M%",salary,0) AS bonus from Employees ORDER BY employee_id;
+
+
+# NOTE: order by is required bcoz it requires records in ascending order of employee_id (seen in wrong submission)
